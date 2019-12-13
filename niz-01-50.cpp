b4ffff7a7dc4dd7cd5726f00e2848c300a8ee768
LarisPauletic <58471911+LarisPauletic@users.noreply.github.com>
2019-12-13 12:48:48 +0100

    Add files via upload

---
+++ niz-01-50.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int i1,i2;
+	for(i1=0;i1<5;i1++){
+		for(i2=0;i2<=9;i2++){
+			cout<<i1<<"-"<<i2<<endl;
+		
+		
+			}
+		}
+	cout<<"5-0"<<endl;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	system("pause");
+	return 0;
+}
---
+++ niz-1-20.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	int i;
+	for(i=1;i<=20;i++){
+		cout<<i;
+	if(i==20){
+		cout<<"."<<endl;
+	}
+	else{
+		cout<<", ";
+	}
+	
+	}	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	system("pause");
+	return 0;
+}
