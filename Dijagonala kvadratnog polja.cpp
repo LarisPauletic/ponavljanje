f37c726416bfb4963cc0db83e967456b0bebd591
LarisPauletic <58471911+LarisPauletic@users.noreply.github.com>
2019-12-17 14:02:33 +0100

    Add files via upload

---
+++ Dijagonala kvadratnog polja.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n,stupac,redak,broj,broj1;
+	cout<<"Unesi broj veci od 2 i manji od 100: ";
+	cin>>n;
+	int p[n];
+	for(stupac=0;stupac<=n;stupac++){
+		p[n]=1;
+	}
+		for(redak=0;redak<=n;redak++){
+			p[n]=1;
+	}
+	cout<<p[n]<<endl;
+	cout<<p[n]<<endl;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	system("pause");
+	return 0;
+}
